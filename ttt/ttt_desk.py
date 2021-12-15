f77d5c911402ce5c0d3b59f237ae5fd94d1c2637
Raycho Mukelov <raicho.mukelov@gmail.com>
2021-12-15 07:14:36 +0200

    added missing files

--- ttt/ttt_desk.py
+++ ttt/ttt_desk.py
@@ -21,7 +21,7 @@ class TTTDesk:
         return possible_moves_indices
 
     def get_state(self):
-        return tuple(tuple(player.get_mark().get_code() if player is not None else None for player in row) for row in self.desk)
+        return tuple(tuple(player.get_mark().get_string() if player is not None else None for player in row) for row in self.desk)
 
     def eval_game_state(self):
         # check rows
