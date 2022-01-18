3c5ce03d8fffe41f21f5a116b9cce288bb194cd9
Raycho Mukelov <raicho.mukelov@gmail.com>
2022-01-18 06:43:46 +0200

    Some try to make it with multiprocessing and multithreading and Postgres connection pooling at the same time

--- ttt/ttt_main.py
+++ ttt/ttt_main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import os
-from multiprocessing import Pool, Manager
-from multiprocessing.managers import BaseManager
+from threading import Thread
+from multiprocessing import Pool
 
 import logging
 from dynaconf import settings
-
+import psycopg2.pool
 
 import ttt_play
 import ttt_game_type
@@ -13,21 +13,16 @@ import ttt_train_data
 import ttt_data_encoder
 import ttt_dependency_injection
 
+from threading import Semaphore
 
 logging.basicConfig(level = logging.INFO, filename = "TTTpid-{}.log".format(os.getpid()),
                     filemode = 'a+',
-                    format='[%(asctime)s] pid: %(process)d - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s() - %(message)s')
+                    format='[%(asctime)s] pid: %(process)d - tid: %(thread)d - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s() - %(message)s')
 logger = logging.getLogger(__name__)
 
 
-class TTTManager(BaseManager):
-    pass
-
-def init_process_pool_manager():
-    TTTManager.register('TTTTrainDataPostgres', ttt_train_data.TTTTrainDataPostgres)
-
 def init_dep_injection():
-    ttt_dependency_injection.DependencyInjection.add_dependency(TTTManager, singleton=True)
+    # ttt_dependency_injection.DependencyInjection.add_dependency(TTTManager, singleton=True)
     if settings.ENCODE_TRAIN_DATA:
         ttt_dependency_injection.DependencyInjection.add_dependency(ttt_data_encoder.TTTDataEncoderMsgpack)
     else:
@@ -35,42 +30,96 @@ def init_dep_injection():
     ttt_dependency_injection.DependencyInjection.add_dependency(ttt_train_data.TTTTrainData, default_args=(), default_kwargs={})
 
 
+class ReallyThreadedPGConnectionPool(psycopg2.pool.ThreadedConnectionPool):
+    def __init__(self, minconn, maxconn, *args, **kwargs):
+        self._semaphore = Semaphore(maxconn)
+        super().__init__(minconn, maxconn, *args, **kwargs)
+
+    def getconn(self, *args, **kwargs):
+        self._semaphore.acquire()
+        return super().getconn(*args, **kwargs)
+
+    def putconn(self, *args, **kwargs):
+        super().putconn(*args, **kwargs)
+        self._semaphore.release()
+
+
 class TTTMain():
-    def __init__(self, iterations):
-        self.concurrency = settings.PROCESS_POOL_SIZE if settings.PROCESS_POOL_SIZE!=0 else os.cpu_count()
-        self.process_managers = [TTTManager() for _ in range(self.concurrency)]
-        [m.start() for m in self.process_managers]
-        logger.info("IPC manager started")
+    def __init__(self, inner_iterations, game_type, train, board_size, concurrency, dbname, user, password, host, port):
+        self.n_iter_info_skip = settings.TRAIN_ITERATIONS_INFO_SKIP
+        self.train = train
+        self.concurrency = concurrency
+        self.board_size = board_size
         logger.info(f"Desk size: {settings.BOARD_SIZE}")
         logger.info(f"Game type: {settings.GAME_TYPE}")
         logger.info(f"Train: {settings.TRAIN}")
-        self.training_data_shared = [self.process_managers[i].TTTTrainDataPostgres(settings.BOARD_SIZE) for i in range(self.concurrency)]
-        self.iterations = iterations
+        tp_conn_count = self.concurrency // 2 or 1
+        self.posgres_conn_pool_threaded = ReallyThreadedPGConnectionPool(1, tp_conn_count , f"dbname={dbname} user={user} password={password} host={host} port={port}")
+        self.training_data_shared = [ttt_train_data.TTTTrainDataPostgres(self.board_size, self.posgres_conn_pool_threaded) for _ in range(self.concurrency)]
+        self.inner_iterations = inner_iterations
+        self.game_type = game_type
 
     def run(self):
-        res = []
-        game_type = ttt_game_type.game_type_factory(settings.GAME_TYPE)
-        if game_type is ttt_game_type.TTTGameTypeCVsC and settings.TRAIN:
-            instances = [ttt_play.TTTPlay(settings.BOARD_SIZE, self.training_data_shared[instance], game_type, settings.TRAIN, train_iterations=settings.INNER_ITERATIONS,
-                                        n_iter_info_skip=settings.TRAIN_ITERATIONS_INFO_SKIP,) for instance in range(settings.PROCESS_POOL_SIZE if settings.PROCESS_POOL_SIZE!=0 else os.cpu_count())]
-            for _ in range(self.iterations):
-                with Pool(settings.PROCESS_POOL_SIZE if settings.PROCESS_POOL_SIZE !=0 else os.cpu_count()) as pool:
-                    for instance in instances:
-                            res = [pool.apply_async(instance.run)]
-                    pool.close()
-                    pool.join()
-            # instance = ttt_play.TTTPlay(settings.BOARD_SIZE, self.training_data_shared[0], game_type, settings.TRAIN, train_iterations=settings.INNER_ITERATIONS,
-            #                              n_iter_info_skip=settings.TRAIN_ITERATIONS_INFO_SKIP)
-            # for _ in range(self.iterations):
-            #     instance.run()
+        logger.debug("TTTMain::run()")
+        game_type = self.game_type
+        if game_type is ttt_game_type.TTTGameTypeCVsC and self.train:
+            instances = [ttt_play.TTTPlay(self.board_size, self.training_data_shared[_], game_type, self.train, self.inner_iterations,
+                                        self.n_iter_info_skip) for _ in range(self.concurrency)]
+            threads = [Thread(target=play.run) for play in instances]
+            for thread in threads:
+                thread.start()
+            for thread in threads:
+                thread.join()
         else:
-            instance = ttt_play.TTTPlay(settings.BOARD_SIZE, ttt_train_data.TTTTrainDataPostgres(settings.BOARD_SIZE), game_type, settings.TRAIN, train_iterations=settings.INNER_ITERATIONS,
-                                         n_iter_info_skip=settings.TRAIN_ITERATIONS_INFO_SKIP)
+            instance = ttt_play.TTTPlay(self.board_size, self.training_data_shared[_], game_type, self.train, self.inner_iterations,
+                                        self.n_iter_info_skip)
             instance.run()
 
 
+class MainProcessPoolRunner:
+    def __init__(self, process_pool_size, iterations, inner_iterations, game_type, train, board_size, threads_count,
+                 dbname, user, password, host, port):
+        self.process_pool_size = process_pool_size
+        self.iterations = iterations
+        self.inner_iterations = inner_iterations
+        self.train = train
+        self.board_size = board_size
+        self.threads_count = threads_count
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.game_type = game_type
+
+
+    def pool_main_run(self):
+        m = TTTMain(self.inner_iterations, self.game_type, self.train, self.board_size, self.threads_count, self.dbname, self.user, self.password, self.host, self.port)
+        m.run()
+
+    def run(self):
+        if self.game_type is ttt_game_type.TTTGameTypeCVsC and self.train:
+            for _ in range(self.iterations):
+                with Pool(self.process_pool_size) as pool:
+                    for _ in range(self.process_pool_size):
+                        pool.apply_async(self.pool_main_run)
+                    pool.close()
+                    pool.join()
+        else:
+            if self.game_type is ttt_game_type.TTTGameTypeCVsC:
+                for _ in range(self.iterations):
+                    ttm = TTTMain(self.iterations, self.game_type, self.train, self.board_size, self.threads_count, self.dbname, self.user, self.password, self.host, self.port)
+                    ttm.run()
+            elif self.game_type is ttt_game_type.TTTGameTypeHVsC:
+                    ttm = TTTMain(self.iterations, self.game_type, self.train, self.board_size, self.threads_count, self.dbname, self.user, self.password, self.host, self.port)
+                    ttm.run()
+            else:
+                raise ValueError("Unknown game type!")
+
+
 if __name__ == "__main__":
-    init_process_pool_manager()
     init_dep_injection()
-    main = TTTMain(settings.ITERATIONS)
-    main.run()
+    game_type = ttt_game_type.game_type_factory(settings.GAME_TYPE)
+    mppr = MainProcessPoolRunner(settings.PROCESS_POOL_SIZE, settings.ITERATIONS, settings.INNER_ITERATIONS, game_type, settings.TRAIN, settings.BOARD_SIZE, settings.THREADS_COUNT,
+                                 settings.POSTGRES_DBNAME, settings.POSTGRES_USER, settings.POSTGRES_PASS, settings.POSTGRES_HOST, settings.POSTGRES_PORT)
+    mppr.run()
--- ttt/ttt_play.py
+++ ttt/ttt_play.py
@@ -15,9 +15,9 @@ import ttt_game_type
 import ttt_dependency_injection
 
 
-logging.basicConfig(level = logging.INFO, filename = "TTTpid-{}.log".format(os.getpid()), 
+logging.basicConfig(level = logging.INFO, filename = "TTTpid-{}.log".format(os.getpid()),
                     filemode = 'a+',
-                    format='[%(asctime)s] pid: %(process)d - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s() - %(message)s')
+                    format='[%(asctime)s] pid: %(process)d - tid: %(thread)d - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s() - %(message)s')
 logger = logging.getLogger(__name__)
 
 
