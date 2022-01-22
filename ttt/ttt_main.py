7bafabc9158de6fae64705a7ac32a6c7c3454629
Raycho Mukelov <raicho.mukelov@gmail.com>
2022-01-22 13:10:06 +0200

    Fix

--- ttt/ttt_main.py
+++ ttt/ttt_main.py
@@ -136,9 +136,9 @@ class MainProcessPoolRunner:
                                     break
                             pool.close()
                             pool.join()
-                    training_data_shared_postgres.inc_total_games_finished(-training_data_shared_postgres.total_games_finished())
-                    training_data_shared_postgres.inc_total_games_finished(training_data_shared_redis.total_games_finished())
-                    # training_data_shared_redis.clear()
+            if settings.MASTER:
+                training_data_shared_postgres.inc_total_games_finished(training_data_shared_redis.total_games_finished())
+                training_data_shared_redis.inc_total_games_finished(-training_data_shared_redis.total_games_finished())
         else:
             if self.game_type is ttt_game_type.TTTGameTypeCVsC:
                 ttm = TTTMain(training_data_shared_postgres, self.iterations, self.inner_iterations, self.n_iter_info_skip, self.game_type, self.train, self.board_size, self.threads_count)
